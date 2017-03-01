dcbd46b50c65c10052cfaee1092c355a7da701ca
Mattia Franchetto <mt.franchetto@gmail.com>
2017-03-01 18:05:05 +0100

    Compete widget registry spec

--- test/WidgetRegistrySpec.ts
+++ test/WidgetRegistrySpec.ts
@@ -1,12 +1,21 @@
 import "reflect-metadata";
 import expect = require("expect.js");
 import * as TypeMoq from "typemoq";
+import WidgetRegistry from "../scripts/registry/WidgetRegistry";
 
 describe("Given a widget registry", () => {
 
+    let subject: WidgetRegistry;
+
+    beforeEach(() => {
+        subject = new WidgetRegistry();
+    });
+
     context("when a new widget is registered", () => {
         context("and the viewmodel is missing", () => {
-            it("should throw an error");
+            it("should throw an error", () => {
+
+            });
         });
 
         context("and the observable factory is missing", () => {
@@ -15,6 +24,16 @@ describe("Given a widget registry", () => {
 
         context("and all the required inputs were given", () => {
             it("should correctly register it");
+        });
+    });
+
+    context("when a widget is requested", () => {
+        context("and it exists", () => {
+            it("should return it");
+        });
+
+        context("and it does not exist", () => {
+            it("should return a null reference");
         });
     });
 });
