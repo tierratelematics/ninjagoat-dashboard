3b7c348ab4d77165c5a43c5bd140661712b82be5
Mattia Franchetto <mt.franchetto@gmail.com>
2017-03-06 17:15:43 +0100

    Add inconclusive model retriever spec

---
+++ test/DashboardModelRetrieverSpec.ts
@@ -0,0 +1,12 @@
+import "reflect-metadata";
+import expect = require("expect.js");
+
+describe("Given DashboardModelRetriever", () => {
+    context("when a dashboard name is provided", () => {
+        it("should retrieve the specific dashboard model");
+    });
+
+    context("when a dashboard name is not provided", () => {
+        it("should retrieve the default model");
+    });
+});
