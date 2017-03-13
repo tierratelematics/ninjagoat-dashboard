a28df1cb7abd42fd713f9adbb73fe069fa9a2bb5
Mattia Franchetto <mt.franchetto@gmail.com>
2017-03-13 18:46:29 +0100

    Refactor dashboard viewmodel

--- scripts/DashboardViewModel.ts
+++ scripts/DashboardViewModel.ts
@@ -9,7 +9,7 @@ import {IDashboardEvents, LayoutItem} from "./DashboardEvents";
 import WidgetSize from "./widget/WidgetSize";
 import * as _ from "lodash";
 import IWidgetSettings from "./widget/IWidgetSettings";
-import {Observable, IDisposable} from "rx";
+import {Observable, IDisposable, IObservable} from "rx";
 import {IReactiveSettingsManager} from "./ReactiveSettingsManager";
 
 export type WidgetItem = [IWidgetSettings<any>, IViewModel<any>];
@@ -52,35 +52,33 @@ export class DashboardViewModel extends ObservableViewModel<ModelState<Dashboard
             if (data.phase === ModelPhase.Ready) {
                 this.failure = null;
                 this.dashboardName = data.model.name;
-                this.constructViewModels(data.model.widgets);
+                this.widgets = _.map(data.model.widgets, widget => this.constructViewModel(widget));
+                this.subscribeToViewModelsChanges();
             } else {
                 this.failure = data.failure;
             }
         }
     }
 
-    private constructViewModels(settings: IWidgetSettings<any>[]) {
-        this.widgets = _.map<IWidgetSettings<any>, WidgetItem>(settings, setting => {
-            let widgetData = _.find(this.widgets, widget => widget[0].id === setting.id);
-            if (widgetData) return widgetData;
-            let widgetEntry = _.find(this.registeredWidgets, widget => widget.name === setting.name);
-            let viewmodelConstructor = widgetEntry.construct;
-            let viewmodelId = this.getViewModelId(this.constructor) + ":" + this.getViewModelId(viewmodelConstructor);
-            return [setting, this.viewmodelFactory.create(
-                new ViewModelContext(this.getDashboardArea(), viewmodelId, setting.configuration),
-                viewmodelConstructor, widgetEntry.observable)];
-        });
-        this.subscribeToViewModelsChanges();
+    private constructViewModel(setting: IWidgetSettings<any>): WidgetItem {
+        let widgetItem = _.find(this.widgets, widget => widget[0].id === setting.id);
+        if (widgetItem)
+            return widgetItem;
+        let entry = _.find(this.registeredWidgets, widget => widget.name === setting.name);
+        let viewmodelName = `${this.getViewModelName(this.constructor)}:${this.getViewModelName(entry.construct)}`;
+        return [setting, this.viewmodelFactory.create(
+            new ViewModelContext(this.areaOfDashboard(), viewmodelName, setting.configuration),
+            entry.construct, entry.observable)];
     }
 
-    private getViewModelId(viewmodel: Function): string {
+    private getViewModelName(viewmodel: Function): string {
         return Reflect.getMetadata("ninjagoat:viewmodel", viewmodel);
     }
 
-    private getDashboardArea(): string {
+    private areaOfDashboard(): string {
         if (this.area)
             return this.area;
-        let area = "";
+        let area = null;
         _.forEach(this.registry.getAreas(), areaRegistry => {
             let entry = _.find(areaRegistry.entries, entry => entry.construct === this.constructor);
             if (entry) area = areaRegistry.area;
@@ -88,23 +86,15 @@ export class DashboardViewModel extends ObservableViewModel<ModelState<Dashboard
         return area;
     }
 
-    @Refresh
-    private refreshView() {
-
-    }
-
     private subscribeToViewModelsChanges() {
         if (this.viewmodelsSubscription)
             this.viewmodelsSubscription.dispose();
-        this.viewmodelsSubscription = Observable.merge(this.getViewModels()).subscribe(change => this.refreshView());
+        this.viewmodelsSubscription = Observable.merge(_.map(this.widgets, widget => widget[1])).subscribe(change => this.refreshView());
     }
 
-    private getViewModels(): IViewModel<any>[] {
-        return _.map(this.widgets, widget => widget[1]);
-    }
+    @Refresh
+    private refreshView() {
 
-    private getSettings(): IWidgetSettings<any>[] {
-        return _.map(this.widgets, widget => widget[0]);
     }
 
     add(name: string, size: WidgetSize) {
@@ -121,7 +111,7 @@ export class DashboardViewModel extends ObservableViewModel<ModelState<Dashboard
     }
 
     private saveSettings() {
-        this.settingsManager.setValueAsync(`ninjagoat.dashboard:${this.dashboardName}`, this.getSettings());
+        this.settingsManager.setValueAsync(`ninjagoat.dashboard:${this.dashboardName}`, _.map(this.widgets, widget => widget[0]));
     }
 
     remove(id: string) {
@@ -137,17 +127,21 @@ export class DashboardViewModel extends ObservableViewModel<ModelState<Dashboard
 
     async configure(id: string) {
         let widgetData = _.find(this.widgets, widget => widget[0].id === id);
-        let viewmodel = <any>widgetData[1];
-        if (viewmodel.configure) {
-            widgetData[0].configuration = await viewmodel.configure();
+        let viewModel = <any>widgetData[1];
+        if (viewModel.configure) {
+            widgetData[0].configuration = await viewModel.configure();
             //Load new observable
-            if (viewmodel.subscription) viewmodel.subscription.dispose();
-            let widgetEntry = _.find(this.registeredWidgets, widget => widget.name === widgetData[0].name);
-            let viewmodelConstructor = widgetEntry.construct;
-            let viewmodelId = this.getViewModelId(this.constructor) + ":" + this.getViewModelId(viewmodelConstructor);
-            widgetEntry.observable(new ViewModelContext(this.getDashboardArea(), viewmodelId, widgetData[0].configuration));
+            if (viewModel.subscription) viewModel.subscription.dispose();
+            let observable = this.observableForConfiguration(widgetData[0].name, widgetData[0].configuration);
+            viewModel.observe(observable);
             this.saveSettings();
         }
+    }
+
+    private observableForConfiguration(widgetName: string, configuration: any): IObservable<any> {
+        let entry = _.find(this.registeredWidgets, widget => widget.name === widgetName);
+        let viewmodelName = `${this.getViewModelName(this.constructor)}:${this.getViewModelName(entry.construct)}`;
+        return entry.observable(new ViewModelContext(this.areaOfDashboard(), viewmodelName, configuration));
     }
 
     dispose() {
