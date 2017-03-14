2d0718c3436ddd6cd04f52d7c2c2f3df4843d11d
Mattia Franchetto <mt.franchetto@gmail.com>
2017-03-14 18:35:09 +0100

    Decrease dashboard size values

--- scripts/DashboardConfig.ts
+++ scripts/DashboardConfig.ts
@@ -18,10 +18,10 @@ type WidgetDimension = {width: number, height: number};
 
 export class DefaultDashboardConfig implements IDashboardConfig {
     sizes = {
-        SMALL: {width: 100, height: 100},
-        MEDIUM: {width: 300, height: 300},
-        LARGE: {width: 500, height: 500}
+        SMALL: {width: 1, height: 1},
+        MEDIUM: {width: 2, height: 2},
+        LARGE: {width: 4, height: 4}
     };
     columns = {lg: 12, md: 10, sm: 6, xs: 4, xxs: 2};
-    rowHeight = 100;
+    rowHeight = 30;
 }
